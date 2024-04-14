a480eb0947918c9d18c22eb564f2974b419a6875
EduardRo <eduardstancu@yahoo.com>
2024-04-14 13:14:29 +0300

    modif vue website section3/12

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,21 +1,5 @@
-import {createApp} from 'vue'
-import CKEditor from '@ckeditor/ckeditor5-vue'
-import store from './store'
-import router from './router'
-import './index.css';
-import currencyUSD from './filters/currency.js'
-
+import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 
-const app = createApp(App);
-
-app
-  .use(store)
-  .use(router)
-  .use(CKEditor)
-  .mount('#app')
-;
-
-app.config.globalProperties.$filters = {
-  currencyUSD
-}
+createApp(App).mount('#app')
